e3f992660a0a8881f6c82c3bef41cb8c26a3ad6f
Anonymous <you@example.com>
2021-11-05 22:32:44 +0600

    started using react route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/orders" element={<OrderReview />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
+    const [products, setProducts] = useProducts();
+
     return (
         <div>
-            <h2>This is Order Review</h2>
+            <h2>Count: {products.length}</h2>
         </div>
     );
 };
