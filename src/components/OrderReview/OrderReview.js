6f1cd03d2f4df00b632ca0b377cdd185a6991d82
Anonymous <you@example.com>
2021-11-09 08:43:49 +0600

    done

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import { clearTheCart, deleteFromDb } from '../../utilities/fakedb';
